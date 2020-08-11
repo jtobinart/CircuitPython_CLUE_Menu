c7d9488300fc0df3654bdb02e1f05d00e37245e4
Mr. James <68863931+jisforjt@users.noreply.github.com>
2020-08-11 13:48:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-# Redirect
+# CircuitPython CLUE main.py
+# Last upated: Aug. 11, 2020, v1.0
+# Author(s): James Tobin
+
+######################################################
+#   Version Notes
+######################################################
+'''
+v1.0
+    - Run a specific file by importing it.
+    - You can add more imports and run a list of programs. 
+      Just remember each file needs to end to run the next one.
+'''
+######################################################
+#   Import
+######################################################
 
 import menu
 
